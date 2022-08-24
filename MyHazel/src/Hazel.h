1563855055c2ca030e4aaa3551da1f1dffbfe4a1
yuymf <1352948945@qq.com>
2022-08-25 02:57:26 +0800

    添加项目文件。

---
+++ MyHazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+// make all include in one file
+
+//For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+// ---Entry Point---------------------- 
+#include "Hazel/EntryPoint.h"
+// ------------------------------------ 
