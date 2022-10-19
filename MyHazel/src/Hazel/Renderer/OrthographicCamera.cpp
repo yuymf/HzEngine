45c037b752536a128ae2c17e0ea6ed2e3c20889f
yuymf <1352948945@qq.com>
2022-10-19 20:31:02 +0800

    #35 // in ortho

--- MyHazel/src/Hazel/Renderer/OrthographicCamera.cpp
+++ MyHazel/src/Hazel/Renderer/OrthographicCamera.cpp
@@ -5,7 +5,7 @@
 
 namespace Hazel {
 	OrthographicCarema::OrthographicCarema(float left, float right, float bottom, float top)
-		:m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
+		:m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)   //near:-1.0f. far: 1.0f
 	{
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
@@ -15,7 +15,7 @@ namespace Hazel {
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Projection) *
 			glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0.0f, 0.0f, 1.0f));    //Rotate by Z-axis
 
-		m_ViewMatrix = glm::inverse(transform);														//逆矩阵；
-		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+		m_ViewMatrix = glm::inverse(transform);														//右逆矩阵；相机与物体运动为逆
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;									//OpenGL: P * V * model * vertexes
 	}
 }
