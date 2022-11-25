2ac111796836b1d219156dcf81f083ef361eb145
yuymf <1352948945@qq.com>
2022-11-25 10:54:07 +0800

    Premake modularization and customization

---
+++ Hazelnut/premake5.lua
@@ -0,0 +1,46 @@
+project "Hazelnut"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/MyHazel/vendor/spdlog/include",
+		"%{wks.location}/MyHazel/src",
+		"%{wks.location}/MyHazel/vendor",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"MyHazel"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		runtime "Release"
+		optimize "on"
--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+
 workspace "MyHazel"
 	architecture "x86_64"
 	startproject "Hazelnut"
@@ -9,6 +11,11 @@ workspace "MyHazel"
 		"Dist"
 	}
 	
+	solution_items
+	{
+		".editorconfig"
+	}
+
 	flags
 	{
 		"MultiProcessorCompile"
@@ -18,184 +25,20 @@ workspace "MyHazel"
 
 --include directories relatave to root folders
 IncludeDir = {}
-IncludeDir["GLFW"] = "MyHazel/vendor/GLFW/include"
-IncludeDir["Glad"] = "MyHazel/vendor/Glad/include"
-IncludeDir["ImGui"] = "MyHazel/vendor/imgui"
-IncludeDir["glm"] = "MyHazel/vendor/glm"
-IncludeDir["stb_image"] = "MyHazel/vendor/stb_image"
-IncludeDir["entt"] = "MyHazel/vendor/entt/include"
+IncludeDir["GLFW"] = "%{wks.location}/MyHazel/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/MyHazel/vendor/Glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/MyHazel/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/MyHazel/vendor/glm"
+IncludeDir["stb_image"] = "%{wks.location}/MyHazel/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/MyHazel/vendor/entt/include"
 
 group "Dependencies"
+	include "vendor/premake"
 	include "MyHazel/vendor/GLFW"
 	include "MyHazel/vendor/Glad"
 	include "MyHazel/vendor/imgui"
 group ""
 
-project "MyHazel"
-	location "MyHazel"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "hzpch.h"
-	pchsource "MyHazel/src/hzpch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl"	
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS",
-		"GLFW_INCLUDE_NONE"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-		}
-
-	filter "configurations:Debug"
-		defines "HZ_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "HZ_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "HZ_DIST"
-		runtime "Release"
-		optimize "on"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"MyHazel/vendor/spdlog/include",
-		"MyHazel/src",
-		"MyHazel/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"MyHazel"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		defines "HZ_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "HZ_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "HZ_DIST"
-		runtime "Release"
-		optimize "on"
-
-project "Hazelnut"
-	location "Hazelnut"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"MyHazel/vendor/spdlog/include",
-		"MyHazel/src",
-		"MyHazel/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"MyHazel"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		defines "HZ_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "HZ_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "HZ_DIST"
-		runtime "Release"
-		optimize "on"
-
-
+include "MyHazel"
+include "Sandbox"
+include "Hazelnut"
